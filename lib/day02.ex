36607538fd0e8647a26a97ad68df93c99ca973a8
Matt Hanlon <matthanlon@gmail.com>
2023-12-03 18:45:36 -0800

    use NimbleParsec on day 2
    what could go wrong?

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,54 +1,55 @@
 defmodule Aoc2023Ex.Day02 do
+  alias ElixirSense.Core.Parser
   use Aoc2023Ex.Day
+
+  defmodule Parser do
+    import NimbleParsec
+    colors = choice([string("red"), string("green"), string("blue")])
+    color_count = wrap(integer(min: 1) |> ignore(string(" ")) |> concat(colors))
+    clause = wrap(color_count |> repeat(concat(ignore(string(", ")), color_count)))
+    clauses = wrap(clause |> repeat(concat(ignore(string("; ")), clause)))
+    line = ignore(string("Game ")) |> integer(min: 1) |> ignore(string(": ")) |> concat(clauses)
+
+    defparsec(:parse_line_help, line)
+
+    def parse_line(line) do
+      {:ok, [gameno, clauses], _, _, _, _} = parse_line_help(line)
+      {gameno, clauses}
+    end
+  end
 
   @colors %{"red" => 12, "green" => 13, "blue" => 14}
 
-  def input_games, do: Enum.map(input_lines(), &parse_line/1)
+  def input_games, do: Enum.map(input_lines(), &Parser.parse_line/1)
 
   def solve1 do
-    input_games()
-    |> Enum.filter(fn {num, clauses} ->
-      Enum.all?(clauses, fn clause ->
-        Enum.all?(clause, fn {color, count} ->
-          count <= @colors[color]
-        end)
-      end)
-    end)
-    |> Enum.map(fn {num, _} -> num end)
+    for {num, clauses} <- input_games(),
+        Enum.all?(clauses, fn clause ->
+          Enum.all?(clause, fn [count, color] ->
+            count <= @colors[color]
+          end)
+        end) do
+      num
+    end
     |> Enum.sum()
   end
 
   def solve2 do
-    input_games()
-    |> Enum.map(fn {_num, clauses} ->
-       Enum.reduce(clauses, %{}, fn clause, acc ->
-         Enum.reduce(clause, acc, fn {color, count}, acc ->
-           {_, acc} = Map.get_and_update(acc, color, fn c ->
-             {c, Enum.max([c || 0, count])}
-           end)
-           acc
-         end)
-       end)
-       |> Map.values()
-       |> Enum.product()
-    end)
+    for {_, clauses} <- input_games() do
+      for clause <- clauses, reduce: %{} do
+        acc ->
+          for [count, color] <- clause, reduce: acc do
+            acc ->
+              {_, map} = Map.get_and_update(acc, color, fn c ->
+                {c, Enum.max([c || 0, count])}
+              end)
+              map
+
+          end
+      end
+      |> Map.values()
+      |> Enum.product()
+    end
     |> Enum.sum()
-  end
-
-  def parse_line(<<"Game ", rest::binary>>) do
-    [num, stuff] = String.split(rest, ": ", parts: 2)
-    clauses = String.split(stuff, "; ")
-
-    clauses =
-      Enum.map(clauses, fn c ->
-        String.split(c, ", ")
-        |> Enum.map(fn s ->
-          String.split(s, " ")
-          |> then(fn [num, color] -> {color, String.to_integer(num)} end)
-        end)
-        |> Map.new()
-      end)
-
-    {String.to_integer(num), clauses}
   end
 end
