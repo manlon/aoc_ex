1be2c4fe0a7d32b1fae45c135581254b112d1838
Matt Hanlon <matthanlon@gmail.com>
2023-12-04 17:51:04 -0800

    day 3 tweak

--- lib/day03.ex
+++ lib/day03.ex
@@ -5,35 +5,28 @@ defmodule Aoc2023Ex.Day03 do
   @dot "."
 
   def solve1 do
-    {map, {rows, cols}} = input_map_with_size()
-    {ints, syms} = find_ints(map, {0, 0}, {rows, cols}, [], %{})
+    {ints, syms} = parse_map()
 
-    for {i, {{r, c1}, {r, c2}}} <- ints,
-        {{sr, sc}, _} <- syms,
-        sr in (r - 1)..(r + 1),
-        sc in (c1 - 1)..(c2 + 1) do
-      i
+    for {loc, _} <- syms, the_int <- ints[loc], reduce: MapSet.new() do
+      map ->
+        MapSet.put(map, the_int)
     end
-    |> Enum.sum()
+    |> Enum.reduce(0, fn {n, _}, acc -> acc + n end)
   end
 
   def solve2 do
-    {map, {rows, cols}} = input_map_with_size()
-    {ints, syms} = find_ints(map, {0, 0}, {rows, cols}, [], %{})
+    {ints, syms} = parse_map()
 
-    for {{gr, gc}, "*"} <- syms do
-      gear_ints =
-        for {i, {{r, c1}, {r, c2}}} <- ints,
-            gr in (r - 1)..(r + 1),
-            gc in (c1 - 1)..(c2 + 1) do
-          i
-        end
-
-      gear_ints
+    for {loc, "*"} <- syms, hits = ints[loc], length(hits) == 2, reduce: 0 do
+      acc ->
+        [{n1, _}, {n2, _}] = hits
+        n1 * n2 + acc
     end
-    |> Enum.filter(fn ints -> length(ints) == 2 end)
-    |> Enum.map(&Enum.product/1)
-    |> Enum.sum()
+  end
+
+  def parse_map() do
+    {map, {rows, cols}} = input_map_with_size()
+    find_ints(map, {0, 0}, {rows, cols}, %{}, %{})
   end
 
   def find_ints(map, {row, col}, {rowmax, colmax}, acc, syms) when col > colmax do
@@ -53,7 +46,16 @@ defmodule Aoc2023Ex.Day03 do
 
       char in @nums ->
         {i, {r, c}} = consume_int(map, {row, col}, {rowmax, colmax}, [])
-        find_ints(map, {r, c + 1}, {rowmax, colmax}, [{i, {{row, col}, {r, c}}} | acc], syms)
+
+        acc =
+          for adjr <- Enum.max([0, r - 1])..Enum.min([r + 1, rowmax]),
+              adjc <- Enum.max([0, col - 1])..Enum.min([c + 1, colmax]),
+              reduce: acc do
+            acc ->
+              Map.update(acc, {adjr, adjc}, [{i, {row, col}}], &[{i, {row, col}} | &1])
+          end
+
+        find_ints(map, {r, c + 1}, {rowmax, colmax}, acc, syms)
 
       true ->
         find_ints(map, {row, col + 1}, {rowmax, colmax}, acc, Map.put(syms, {row, col}, char))
