b93e35602adb66efcc820f44ec8ae9093e233bd0
Matt Hanlon <matthanlon@gmail.com>
2023-12-06 12:55:34 -0800

    day 6 tweak

--- lib/day06.ex
+++ lib/day06.ex
@@ -8,13 +8,12 @@ defmodule Aoc2023Ex.Day06 do
     dists = istr("Distance: ") |> ispace() |> concat(line_of_ints)
     defmatch(:parse, wrap(times) |> istr("\n") |> concat(wrap(dists)))
 
-    def parse_input(input) do
-      parse(input)
-      |> Enum.zip()
+    def parsed_input() do
+      Enum.zip(parse(Aoc2023Ex.Day06.input()))
     end
 
-    def parse_input_2(input) do
-      for list <- parse(input) do
+    def parsed_input_2() do
+      for list <- parse(Aoc2023Ex.Day06.input()) do
         Enum.flat_map(list, &Integer.digits/1)
         |> Integer.undigits()
       end
@@ -22,23 +21,21 @@ defmodule Aoc2023Ex.Day06 do
     end
   end
 
-  def dist_with_time(hold_time, race_time) do
-    hold_time * (race_time - hold_time)
-  end
-
-  def num_wins(t, dist) do
-    0..t
-    |> Enum.count(fn n -> dist_with_time(n, t) > dist end)
+  def num_wins(time, dist) do
+    z1 = (-time + (time ** 2 - 4 * dist) ** 0.5) / -2
+    z2 = (-time - (time ** 2 - 4 * dist) ** 0.5) / -2
+    floor(z2) - ceil(z1) + 1
   end
 
   def solve1 do
-    Parser.parse_input(input())
-    |> Enum.map(fn {t, dist} -> num_wins(t, dist) end)
+    for {t, dist} <- Parser.parsed_input() do
+      num_wins(t, dist)
+    end
     |> Enum.product()
   end
 
   def solve2 do
-    {time, dist} = Parser.parse_input_2(input())
+    {time, dist} = Parser.parsed_input_2()
     num_wins(time, dist)
   end
 end
