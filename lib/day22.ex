0710e2dca03844980982d38753a530183c749246
Matt Hanlon <matthanlon@gmail.com>
2024-12-30 21:21:04 -0500

    day22 wip

--- lib/day22.ex
+++ lib/day22.ex
@@ -10,32 +10,76 @@ defmodule Aoc2023Ex.Day22 do
     def parsed_input do
       Aoc2023Ex.Day22.input_lines()
       |> Enum.map(&parse_line/1)
-      |> Enum.map(fn [[x1, y1, z1], [x2, y2, z2]] -> {x1..x2, y1..y2, z1..z2} end)
+      |> Enum.map(&Enum.map(&1, fn x -> List.to_tuple(x) end))
+      |> Enum.map(&List.to_tuple/1)
     end
   end
 
-  def overlap?(brick1 = {rx1, ry1, rz1}, brick2 = {rx2, ry2, rz2}) do
-    [{rx1, rx2}, {ry1, ry2}, {rz1, rz2}]
-    |> Enum.all?(fn {r1, r2} -> !Range.disjoint?(r1, r2) end)
+  def z, do: Parser.parsed_input()
+
+  def bottom_points(brick = {{x1, y1, z1}, {x2, y2, z2}}) do
+    for x <- x1..x2 do
+      for y <- y1..y2 do
+        {x, y, z1}
+      end
+    end
+    |> List.flatten()
   end
 
-  def units([[x1, y1, z1], [x2, y2, z2]]) do
-    for x <- x1..x2, y <- y1..y2, z <- z1..z2, do: {x, y, z}
+  def lowest(brick = {{_, _, z}, _}), do: z
+
+  def fall_point({x, y, z}), do: {x, y, z - 1}
+
+  def has_point?(_brick = {{x1, y1, z1}, {x2, y2, z2}}, _pt = {xp, yp, zp}) do
+    xp in x1..x2 && yp in y1..y2 && zp in z1..z2
   end
 
-  def ranges([[x1, y1, z1], [x2, y2, z2]]) do
-    {x1..x2, y1..y2, z1..z2}
+  def atop?(b1, b2) do
+    bottom_points(b1)
+    |> Enum.map(&fall_point/1)
+    |> Enum.any?(&has_point?(b2, &1))
   end
 
-  def fall(brick = {rx, ry, z1..z2}) do
-    if z1 > 1 do
-      {rx, ry, (z1 - 1)..(z2 - 1)}
+  def lock(bricks, settled) do
+    num_settled = length(settled)
+
+    {unsettled, settled} =
+      Enum.reduce(bricks, {[], settled}, fn brick, {unsettled, settled} ->
+        if lowest(brick) == 1 do
+          {unsettled, [brick | settled]}
+        else
+          if Enum.any?(settled, &atop?(brick, &1)) do
+            {unsettled, [brick | settled]}
+          else
+            {[brick | unsettled], settled}
+          end
+        end
+      end)
+
+    if length(settled) == num_settled do
+      {unsettled, settled}
     else
-      brick
+      lock(unsettled, settled)
     end
+  end
+
+  # assume bricks are not locked
+  # def fall([brick | rest], settled, acc) do
+  #   if(Enum.any?())
+  # end
+
+  def can_settle?(bricks) do
+    {bricks, _} = lock(bricks, [])
+    Enum.any?(bricks)
+  end
+
+  def fall(bricks, settled) do
   end
 
   def solve1 do
+    pts =
+      Parser.parsed_input()
+      |> then(fn bricks -> lock(bricks, []) end)
   end
 
   def solve2 do
